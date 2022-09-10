c8b365ca81e79c14ce902dddf97500f1343d3991
aurelien3333 <aurelienverdeau@gmail.com>
2022-09-10 10:55:01 +0000

    commit install breeze fail

--- resources/views/livewire/test.blade.php
+++ resources/views/livewire/test.blade.php
@@ -1,30 +1,34 @@
 
 
 <div class="h-screen pt-12 mx-auto bg-gray-300">
-    <div class='max-w-4xl p-4 mx-auto mt-12 bg-white'>
+    <div class='max-w-4xl p-4 mx-auto mt-12 bg-white border border-black rounded-md shadow-md'>
        <div>Question {{$step}} / {{$maxStep}}</div>
-        <div class="my-4 text-center">
+        <div class="h-12 my-4 text-center">
             {{$question['question']}}
         </div>
 
-        <div class="flex flex-col justify-between">
+        <div class="flex flex-col justify-between my-12">
 
-            <div class="grid grid-cols-3 gap-4">
-                @foreach ($reponses as $index => $reponse)
-                <div class="btn" wire:click='reponse({{$index + 1}})'>{{$reponse}}</div>
+            <div class="flex justify-between px-24">
+                @foreach ($reponses as $reponse)
+                    <div>
+                        <div class="w-16 h-16 border border-gray-600 rounded-full shadow-xl cursor-pointer" wire:click='reponse({{$reponse['id']}})' style="background-color:{{$reponse['couleur']}};"></div>
+                        <div class="text-[10px] font-bold text-center mt-2 w-16">{{$reponse['reponse']}}</div>
+                    </div>
                 @endforeach
 
             </div>
-            <div class="flex flex-row justify-around mt-4">
+            <div class="flex flex-row justify-around mt-12">
                 <div class="btn" wire:click="previous">Précedent</div>
                 <div>
                     @if($step == $this->maxStep)
                         <div class="btn" wire:click="finish">Terminer</div>
                     @else
-                        <div class="bg-green-500 btn" wire:click="next">Suivant</div>
+                        <div class="text-white bg-green-600 border border-green-700 shadow-xl btn" wire:click="next">Suivant</div>
                     @endif
                 </div>
             </div>
+
 
 
         </div>
