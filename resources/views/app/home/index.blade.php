5a35fb6e52538655f4748e4eb7d6065d33a78dce
aurelien3333 <aurelienverdeau@gmail.com>
2022-09-18 11:03:23 +0000

    wip mav

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -1,8 +1,11 @@
-@extends('layouts.base')
-
-@section('base.content')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 <div>
     page home
 
 </div>
-@endsection
+</x-app-layout>
